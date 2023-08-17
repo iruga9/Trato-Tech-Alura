59ab3c22906fca004988e1a7f0641bd53c3227e1
Hamilton Teixeira <iruga211@gmail.com>
2023-08-17 17:41:11 -0300

    Add files via upload

---
+++ src/components/Header/index.js
@@ -0,0 +1,24 @@
+import styles from './Header.module.scss';
+import TituloSemImagem from './TituloSemImagem';
+import TituloComImagem from './TituloComImagem';
+
+export default function Header({ titulo, descricao, className = '', imagem}) {
+  return (
+    <header className={`${styles.header} ${className}`}>
+      {titulo && !imagem &&
+        <TituloSemImagem 
+          titulo={titulo}
+          descricao={descricao}
+        />
+      }
+      {titulo && imagem &&
+        <TituloComImagem 
+          titulo={titulo}
+          descricao={descricao}
+          imagem={imagem}
+          className={className}
+        />
+      }
+    </header>
+  )
+}
---
+++ src/pages/Categoria/index.js
@@ -0,0 +1,34 @@
+import Header from "components/Header";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import styles from "./Categoria.module.scss";
+import Item from "components/Item";
+
+function Categoria() {
+    const { nomeCategoria } = useParams();
+    const { categoria, itens } = useSelector(state => {
+      const regexp = new RegExp(state.busca, 'i');
+       return { 
+        categoria: state.categorias.find(categoria => categoria.id === nomeCategoria),
+        itens: state.itens.filter(item => item.categoria === nomeCategoria && item.titulo.match(regexp))
+       }
+    });
+    return (
+        <div>
+            <Header 
+                titulo={categoria.nome}
+                descricao={categoria.descricao}
+                imagem={categoria.header}
+            />
+            <div className={styles.itens}>
+                {itens?.map(item => (
+                    <div key={item.id}>
+                        <Item key={item.id} {...item}/>
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default Categoria;
